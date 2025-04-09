e2238c229968b7ac6b89417043d81add43039f8f
LAPTOP-5DSTQPA3\workb <sho@synicsw.com>
2025-04-08 20:43:04 -0700

    add

--- ans/01-ocp.ts
+++ ans/01-ocp.ts
@@ -1,7 +1,31 @@
 (function () {
+  interface ITax {
+    rate: number;
+  }
+
+  interface ITaxCalculator {
+    calcTax(tax: ITax, amount: number): number;
+  }
+
   // follow OCP?
-  function calcTax(tax, amount) {
-    return amount * tax.rate;
+  function calcTax(
+    calculator: ITaxCalculator,
+    tax: ITax,
+    amount: number
+  ): number {
+    return calculator.calcTax(tax, amount);
+  }
+
+  class GstTaxCalculator implements ITaxCalculator {
+    calcTax(tax: ITax, amount: number): number {
+      return amount * tax.rate;
+    }
+  }
+
+  class PstTaxCalculator implements ITaxCalculator {
+    calcTax(tax: ITax, amount: number): number {
+      return amount * tax.rate;
+    }
   }
 
   // from db
@@ -12,6 +36,9 @@
     rate: 0.07,
   };
 
-  const gstTaxAmount = calcTax(gstTax, 100);
-  const pstTaxAmount = calcTax(gstTax, 100);
+  const gstTaxAmount = calcTax(new GstTaxCalculator(), gstTax, 100);
+  const pstTaxAmount = calcTax(new PstTaxCalculator(), pstTax, 100);
+
+  console.log(`GST Tax Amount: ${gstTaxAmount.toFixed(2)}`); // GST Tax Amount: 5
+  console.log(`PST Tax Amount: ${pstTaxAmount.toFixed(2)}`); // PST Tax Amount: 7
 })();
